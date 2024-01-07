e319767f3339e633a237c0a3e3d363396f0fcf37
Alex Pogostin <alex.pogostin@uncodedigital.com>
2024-01-07 00:50:57 -0600

    changed the name of the RX buffer register offset to match TI convension

--- interrupts.c
+++ interrupts.c
@@ -13,7 +13,7 @@
 /*****************************************************************************/
 /* global declarations                                                       */
 /*****************************************************************************/
-#define RXBUF_REGISTER 0x0C
+#define OFS_UCA0RXBUF 0x0C
 /*****************************************************************************/
 /* externs - these global variables are declared in tasks.c                  */
 /*****************************************************************************/
@@ -90,7 +90,7 @@ __interrupt void USCIA0Interrupt(void)
         break;
 
         case 0x02: // RX interrupt
-            rxReg = read_reg_8(EUSCI_A0_BASE + RXBUF_REGISTER);
+            rxReg = read_reg_8(EUSCI_A0_BASE + OFS_UCA0RXBUF);
             if(i < UART_RX_BUF_SIZE - 1)
             {
                 if(rxReg == BACKSPACE)
